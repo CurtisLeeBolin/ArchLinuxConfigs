a934fb6a7032ebe385315360c78b52c218ce63fd
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2014-04-27 08:58:15 -0500

    Update addDefaultSettings.sh

--- addDefaultSettings.sh
+++ addDefaultSettings.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-for each in .bashrc .nanorc .profile .screenrc .yaourtrc; do
+for each in .bashrc .nanorc .profile .tmux.conf .yaourtrc; do
 	curl -O https://raw.githubusercontent.com/CurtisLeeBolin/MyArchLinuxGoodies/master/"$each"
 done
 
 sudo mv .bashrc /etc/bash.bashrc
 sudo mv .nanorc /etc/nanorc
 sudo mv .profile /etc/profile
-sudo mv .screenrc /etc/screenrc
+sudo mv .tmux.conf /etc/tmux.conf
 sudo mv .yaourtrc /etc/yaourtrc
-rm -f $HOME/.bashrc $HOME/.nanorc $HOME/.profile $HOME/.screenrc $HOME/.yaourtrc
-sudo rm -f /root/.bashrc /root/.nanorc /root/.profile /root/.screenrc /root/.yaourtrc
+rm -f $HOME/.bashrc $HOME/.nanorc $HOME/.profile $HOME/.tmux.conf $HOME/.yaourtrc
+sudo rm -f /root/.bashrc /root/.nanorc /root/.profile /root/.tmux.conf /root/.yaourtrc
