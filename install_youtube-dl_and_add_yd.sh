b5d6697456ae94d0964aa1ae7b1109fd5a489f7f
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2014-07-25 12:54:50 -0500

    Update install_youtube-dl_and_add_yd.sh

--- install_youtube-dl_and_add_yd.sh
+++ install_youtube-dl_and_add_yd.sh
@@ -83,7 +83,7 @@ def reporthook(count, block_size, total_size):
     sys.stdout.write("\r...%d%%, %d MB, %d KB/s, %d seconds passed" %
                     (percent, progress_size / (1024 * 1024), speed, duration))
     sys.stdout.flush()
- 
+
 def save(url, filename):
     urllib.urlretrieve(url, filename, reporthook)
 
