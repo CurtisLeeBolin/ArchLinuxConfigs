1e3ee0305389a537755d1ddd4ba270f7961aef68
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2014-08-23 09:47:46 -0500

    Update addDefaultSettings.sh

--- scripts/addDefaultSettings.sh
+++ scripts/addDefaultSettings.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm --needed bash bash-completion
+sudo pacman -S --noconfirm --needed bash bash-completion nano tmux
 
-for each in .bashrc .nanorc .profile .tmux.conf .yaourtrc; do
-	curl -O https://raw.githubusercontent.com/CurtisLeeBolin/MyArchLinuxGoodies/master/"$each"
-done
-
-sudo mv .bashrc /etc/bash.bashrc
-sudo mv .nanorc /etc/nanorc
-sudo mv .profile /etc/profile
-sudo mv .tmux.conf /etc/tmux.conf
-sudo mv .yaourtrc /etc/yaourtrc
+sudo cp ../configs/.bashrc /etc/bash.bashrc
+sudo mv ../configs/.nanorc /etc/nanorc
+sudo mv ../configs/.profile /etc/profile
+sudo mv ../configs/.tmux.conf /etc/tmux.conf
+sudo mv ../configs/.yaourtrc /etc/yaourtrc
 rm -f $HOME/.bashrc $HOME/.nanorc $HOME/.profile $HOME/.tmux.conf $HOME/.yaourtrc
 sudo rm -f /root/.bashrc /root/.nanorc /root/.profile /root/.tmux.conf /root/.yaourtrc
