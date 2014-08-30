9af4ad5a355b1cd9d23e8d65d455ed8935eb593b
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2014-08-30 18:10:54 -0500

    Update addDefaultSettings.sh

--- scripts/addDefaultSettings.sh
+++ scripts/addDefaultSettings.sh
@@ -9,9 +9,10 @@ fi
 sudo pacman -S --noconfirm --needed bash bash-completion nano tmux
 
 sudo cp ../configs/.bashrc /etc/bash.bashrc
-sudo mv ../configs/.nanorc /etc/nanorc
-sudo mv ../configs/.profile /etc/profile
-sudo mv ../configs/.tmux.conf /etc/tmux.conf
-sudo mv ../configs/.yaourtrc /etc/yaourtrc
+sudo cp ../configs/.nanorc /etc/nanorc
+sudo cp ../configs/.profile /etc/profile
+sudo cp ../configs/.tmux.conf /etc/tmux.conf
+sudo cp ../configs/.yaourtrc /etc/yaourtrc
 rm -f $HOME/.bashrc $HOME/.nanorc $HOME/.profile $HOME/.tmux.conf $HOME/.yaourtrc
 sudo rm -f /root/.bashrc /root/.nanorc /root/.profile /root/.tmux.conf /root/.yaourtrc
+sudo rm /etc/skel/*
