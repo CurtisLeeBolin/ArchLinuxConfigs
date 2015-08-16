0f8bb2e65983b0b065d88126b25f766b6572d9cd
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2015-08-16 16:16:37 -0500

    Update ydp.sh

--- scripts/ydp.sh
+++ scripts/ydp.sh
@@ -3,4 +3,6 @@
 # Downloads all videos of a playlist(s)
 # ydp <link> <link> ...
 
-youtube-dl --continue --format="bestvideo[height<=?720]+bestaudio/best" --audio-format vorbis --embed-subs --embed-thumbnail --add-metadata --merge-output-format mkv --ignore-errors --sub-lang en --write-sub --user-agent "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36" --output "%(autonumber)s %(title)s (%(id)s).%(ext)s" "$@"
+USER_AGENT="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+
+youtube-dl --continue --format="bestvideo[height<=?720]+bestaudio/best" --audio-format vorbis --embed-subs --embed-thumbnail --add-metadata --merge-output-format mkv --ignore-errors --sub-lang en --write-sub --user-agent "$USER_AGENT" --output "%(autonumber)s %(title)s (%(id)s).%(ext)s" "$@"
