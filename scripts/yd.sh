189299b431ff8ae7dd14df46759d71fad8144f54
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2015-08-16 16:02:37 -0500

    Create yd.sh

---
+++ scripts/yd.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# yd
+# Simplifies the use of youtube-dl (works with all site not just youtube)
+# yd "<link> <link> ..."
+
+url=$@
+
+download_video () {
+	youtube-dl --continue --format="bestvideo[height<=?720]+bestaudio/best" --audio-format vorbis --embed-subs --embed-thumbnail --add-metadata --merge-output-format mkv --ignore-errors --sub-lang en --write-sub --user-agent "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.130 Safari/537.36" --output "%(title)s (%(id)s).%(ext)s" "$1"
+}
+
+if [[ "$url" =~ ( |\') ]]; then
+  arr=($url)
+  for each in "${arr[@]}"; do
+    download_video $each
+  done
+else
+  download_video $url
+fi
