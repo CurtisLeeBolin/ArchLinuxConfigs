dc9266595bbc5f0b74268659b67e08c7e9c21429
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2015-08-16 16:17:13 -0500

    Update yds.sh

--- scripts/yds.sh
+++ scripts/yds.sh
@@ -9,6 +9,8 @@
 # or 
 # yds 10 "Harlem Shake"
 
+USER_AGENT="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+
 count="$1"
 shift
-youtube-dl --continue --format="bestvideo[height<=?720]+bestaudio/best" --audio-format vorbis --embed-subs --embed-thumbnail --add-metadata --merge-output-format mkv --ignore-errors --sub-lang en --write-sub --user-agent "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36" --output "%(title)s (%(id)s).%(ext)s" "ytsearch$count:$@"
+youtube-dl --continue --format="bestvideo[height<=?720]+bestaudio/best" --audio-format vorbis --embed-subs --embed-thumbnail --add-metadata --merge-output-format mkv --ignore-errors --sub-lang en --write-sub --user-agent "$USER_AGENT" --output "%(title)s (%(id)s).%(ext)s" "ytsearch$count:$@"
