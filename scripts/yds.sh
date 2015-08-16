129fdbb291a5d34cca5bb3254dfa386f6441ecb9
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2015-08-16 16:05:23 -0500

    Create yds.sh

---
+++ scripts/yds.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# yds
+# Downloads from a youtube search
+# yds <number> <search terms>
+#
+# examples to download 10 Harlem Shake Videos:
+#
+# yds 10 Harlem\ Shake
+# or 
+# yds 10 "Harlem Shake"
+
+count="$1"
+shift
+youtube-dl --continue --format="bestvideo[height<=?720]+bestaudio/best" --audio-format vorbis --embed-subs --embed-thumbnail --add-metadata --merge-output-format mkv --ignore-errors --sub-lang en --write-sub --user-agent "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36" --output "%(title)s (%(id)s).%(ext)s" "ytsearch$count:$@"
