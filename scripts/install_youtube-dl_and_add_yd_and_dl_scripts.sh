5425b8c7f691e40fbd3307b06af7a8a754a7cc3b
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2015-08-16 16:13:15 -0500

    Update install_youtube-dl_and_add_yd_and_dl_scripts.sh

--- scripts/install_youtube-dl_and_add_yd_and_dl_scripts.sh
+++ scripts/install_youtube-dl_and_add_yd_and_dl_scripts.sh
@@ -4,77 +4,8 @@
 
 sudo pacman -Sy --noconfirm --needed youtube-dl rtmpdump python ffmpeg aria2
 
-USER_AGENT="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
-
-
-cat <<EOF | sudo tee /usr/local/bin/yd
-#!/bin/bash
-# yd
-# Simplifies the use of youtube-dl (works with all site not just youtube)
-# yd <link> <link> ...
-
-youtube-dl --continue --format="bestvideo[height<=?720]+bestaudio/best" --audio-format vorbis --embed-subs --embed-thumbnail --add-metadata --merge-output-format mkv --ignore-errors --sub-lang en --write-sub --user-agent "$USER_AGENT" --output "%(title)s (%(id)s).%(ext)s" "\$@"
-EOF
-
-
-cat <<EOF | sudo tee /usr/local/bin/yda
-#!/bin/bash
-# yda
-# Simplifies the use of youtube-dl to download audio from youtube
-# yda <link> <link> ...
-
-youtube-dl --continue --format=bestaudio --ignore-errors --user-agent "$USER_AGENT" --output "%(title)s (%(id)s).%(ext)s" "\$@"
-EOF
-
-
-cat <<EOF | sudo tee /usr/local/bin/ydu
-#!/bin/bash
-# ydu
-# Downloads all videos of a user
-# ydu <username>
-
-youtube-dl --continue --format="bestvideo[height<=?720]+bestaudio/best" --audio-format vorbis --embed-subs --embed-thumbnail --add-metadata --merge-output-format mkv --ignore-errors --sub-lang en --write-sub --user-agent "$USER_AGENT" --output "%(upload_date)s %(title)s (%(id)s).%(ext)s" "http://www.youtube.com/user/\$1"
-EOF
-
-
-cat <<EOF | sudo tee /usr/local/bin/ydp
-#!/bin/bash
-# ydp
-# Downloads all videos of a playlist(s)
-# ydp <link> <link> ...
-
-youtube-dl --continue --format="bestvideo[height<=?720]+bestaudio/best" --audio-format vorbis --embed-subs --embed-thumbnail --add-metadata --merge-output-format mkv --ignore-errors --sub-lang en --write-sub --user-agent "$USER_AGENT" --output "%(autonumber)s %(title)s (%(id)s).%(ext)s" "\$@"
-EOF
-
-
-cat <<EOF | sudo tee /usr/local/bin/yds
-#!/bin/bash
-# yds
-# Downloads from a youtube search
-# yds <number> <search terms>
-#
-# examples to download 10 Harlem Shake Videos:
-#
-# yds 10 Harlem\ Shake
-# or 
-# yds 10 "Harlem Shake"
-
-count="\$1"
-shift
-youtube-dl --continue --format="bestvideo[height<=?720]+bestaudio/best" --audio-format vorbis --embed-subs --embed-thumbnail --add-metadata --merge-output-format mkv --ignore-errors --sub-lang en --write-sub --user-agent "$USER_AGENT" --output "%(title)s (%(id)s).%(ext)s" "ytsearch\$count:\$@"
-EOF
-
-
-cat <<EOF | sudo tee /usr/local/bin/dl
-#!/bin/bash
-# dl
-# Simplifies the use of axel
-# dl <link> <link> ...
-
-for url in \$@; do
-  aria2c --max-concurrent-downloads=10 --continue=true --user-agent="$USER_AGENT" "\$url"
+script_arr=(dl yd yda ydp yds ydu)
+for each in "${script_arr[@]}"; do
+  sudo cp ./"$each".sh  /usr/local/bin/"$each"
+  sudo chmod +x /usr/local/bin/"$each"
 done
-EOF
-
-
-sudo chmod +x /usr/local/bin/yd /usr/local/bin/yda /usr/local/bin/yds /usr/local/bin/ydp /usr/local/bin/ydu /usr/local/bin/dl
