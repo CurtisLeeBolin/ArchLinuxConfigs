be2ff68087d3e8c1778f8128fee263d74eafd38c
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2015-08-16 16:15:47 -0500

    Update yda.sh

--- scripts/yda.sh
+++ scripts/yda.sh
@@ -3,10 +3,12 @@
 # Simplifies the use of youtube-dl to download audio from youtube
 # yda "<link> <link> ..."
 
+USER_AGENT="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+
 url=$@
 
 download_video () {
-	youtube-dl --continue --format=bestaudio --ignore-errors --user-agent "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36" --output "%(title)s (%(id)s).%(ext)s" "$1"
+	youtube-dl --continue --format=bestaudio --ignore-errors --user-agent "$USER_AGENT" --output "%(title)s (%(id)s).%(ext)s" "$1"
 }
 
 if [[ "$url" =~ ( |\') ]]; then
