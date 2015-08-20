6ffc9b1c62225a18720c69f18b192923bc194843
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2015-08-20 03:59:03 -0500

    Create ydc.sh

---
+++ scripts/ydc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# ydc
+# Downloads all videos of a channel
+# ydc <channel>
+
+USER_AGENT="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+
+youtube-dl --continue --format="bestvideo[height<=?720]+bestaudio/best" --audio-format vorbis --embed-subs --embed-thumbnail --add-metadata --merge-output-format mkv --ignore-errors --sub-lang en --write-sub --user-agent "$USER_AGENT" --output "%(upload_date)s %(title)s (%(id)s).%(ext)s" "http://www.youtube.com/channel/$1"
