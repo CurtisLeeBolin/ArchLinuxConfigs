d82b33feeb339406b39b377edf16cce31ba2b3a4
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2015-08-20 03:59:51 -0500

    Update install_youtube-dl_and_add_yd_and_dl_scripts.sh

--- scripts/install_youtube-dl_and_add_yd_and_dl_scripts.sh
+++ scripts/install_youtube-dl_and_add_yd_and_dl_scripts.sh
@@ -4,7 +4,7 @@
 
 sudo pacman -Sy --noconfirm --needed youtube-dl rtmpdump python ffmpeg aria2
 
-script_arr=(dl yd yda ydp yds ydu)
+script_arr=(dl yd yda ydc ydp yds ydu)
 for each in "${script_arr[@]}"; do
   sudo cp ./"$each".sh  /usr/local/bin/"$each"
   sudo chmod +x /usr/local/bin/"$each"
