a2db5531145bad7484c2c2cf8715b2127b5e4330
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2018-04-27 13:55:47 -0500

    Update addDefaultSettings.sh
    No longer using yaourt.

--- scripts/addDefaultSettings.sh
+++ scripts/addDefaultSettings.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-# check for yaourt
-if  ! hash yaourt > /dev/null 2>&1; then
-	echo "Please install yaourt first!"
-	exit 1
-fi
-
 sudo pacman -S --noconfirm --needed bash bash-completion nano tmux rsync openssh
 
 sudo cp ../configs/.bashrc /etc/bash.bashrc
 sudo cp ../configs/.nanorc /etc/nanorc
 sudo cp ../configs/.profile /etc/profile
 sudo cp ../configs/.tmux.conf /etc/tmux.conf
-sudo cp ../configs/.yaourtrc /etc/yaourtrc
-rm -f $HOME/.bashrc $HOME/.nanorc $HOME/.profile $HOME/.tmux.conf $HOME/.yaourtrc
-sudo rm -f /root/.bashrc /root/.nanorc /root/.profile /root/.tmux.conf /root/.yaourtrc
+rm -f $HOME/.bashrc $HOME/.nanorc $HOME/.profile $HOME/.tmux.conf
+sudo rm -f /root/.bashrc /root/.nanorc /root/.profile /root/.tmux.conf
 sudo rm /etc/skel/{.bash_logout,.bash_profile,.bashrc}
