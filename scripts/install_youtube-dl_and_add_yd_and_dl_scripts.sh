40996b7b9eb846f92c1314aafe853d63a825c9d4
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2018-10-28 05:18:03 -0500

    Update install_youtube-dl_and_add_yd_and_dl_scripts.sh

--- scripts/install_youtube-dl_and_add_yd_and_dl_scripts.sh
+++ scripts/install_youtube-dl_and_add_yd_and_dl_scripts.sh
@@ -2,7 +2,7 @@
 # add-yd.sh
 # adds my dl and yd scripts for youtube-dl and installs youtube-dl
 
-sudo pacman -Sy --noconfirm --needed youtube-dl rtmpdump python ffmpeg aria2
+sudo pacman -Sy --noconfirm --needed youtube-dl ffmpeg rtmpdump atomicparsley python-crypto aria2
 
 script_arr=(dl yd ydn yda ydc ydp yds ydu)
 for each in "${script_arr[@]}"; do
