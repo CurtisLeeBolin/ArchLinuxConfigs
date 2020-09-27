e389b8c69b8f6a256affed29a278932b21a71b2f
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2020-09-27 12:39:58 -0500

    Some Cleanup

--- scripts/addDefaultSettings.sh
+++ scripts/addDefaultSettings.sh
@@ -4,8 +4,4 @@ sudo pacman -S --noconfirm --needed bash bash-completion nano tmux rsync openssh
 
 sudo cp ../configs/.bashrc /etc/bash.bashrc
 sudo cp ../configs/.nanorc /etc/nanorc
-sudo cp ../configs/.profile /etc/profile
 sudo cp ../configs/.tmux.conf /etc/tmux.conf
-rm -f $HOME/.bashrc $HOME/.nanorc $HOME/.profile $HOME/.tmux.conf
-sudo rm -f /root/.bashrc /root/.nanorc /root/.profile /root/.tmux.conf
-sudo rm /etc/skel/{.bash_logout,.bash_profile,.bashrc}
