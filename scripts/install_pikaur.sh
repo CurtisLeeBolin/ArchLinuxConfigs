6ee5cd3c424e6c41e6e6cbc5c1d28ddbe3b39c45
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2020-09-28 06:39:06 -0500

    Update and rename install_yay.sh to install_pikaur.sh

---
+++ scripts/install_pikaur.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# install_pikaur.sh
+
+sudo pacman -Sy --noconfirm --needed base-devel git
+mkdir -p /tmp/build/
+cd /tmp/build/
+git clone https://aur.archlinux.org/pikaur.git
+cd pikaur/
+makepkg -si --noconfirm
+cd
+rm -rf /tmp/build
+pikaur -D --asexplicit pikaur
