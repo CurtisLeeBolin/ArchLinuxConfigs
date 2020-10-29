4b6dfe26e1c6cdb884746b45def53f696271fdb1
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2020-10-29 09:24:02 -0500

    Update ydc.sh

--- scripts/ydc.sh
+++ scripts/ydc.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # ydc
-# Downloads all videos of a channel
+# Downloads all videos of a /c/ channel
 # ydc <channel>
+
+cookie=$(<~/.config/youtube-dl/cookies.txt)
 
 youtube-dl --continue --embed-subs --embed-thumbnail --add-metadata \
   --merge-output-format mkv --ignore-errors --sub-lang en --write-sub \
-  --age-limit 30 \
+  --cookies ~/.config/youtube-dl/cookies.txt \
   --output "%(upload_date)s %(title)s (%(id)s).%(ext)s" \
-  "http://www.youtube.com/channel/$1"
+  "http://www.youtube.com/c/$1"
