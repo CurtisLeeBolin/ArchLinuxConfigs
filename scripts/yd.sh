568b2403998fc408e93f69f76facc2f56ae3605c
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2020-10-29 12:53:47 -0500

    Update yd.sh

--- scripts/yd.sh
+++ scripts/yd.sh
@@ -6,10 +6,10 @@
 url=$@
 
 download_video () {
-	youtube-dl --continue --format bestvideo+bestaudio \
-		--merge-output-format mkv --ignore-errors \
-		--age-limit 30 \
-		--output "%(title)s (%(id)s).%(ext)s" "$1"
+  youtube-dl --continue --embed-subs --embed-thumbnail --add-metadata \
+    --merge-output-format mkv --ignore-errors --sub-lang en --write-sub \
+    --cookies ~/.config/youtube-dl/cookies.txt \
+    --output "%(title)s (%(id)s).%(ext)s" "$1"
 }
 
 if [[ "$url" =~ ( |\') ]]; then
