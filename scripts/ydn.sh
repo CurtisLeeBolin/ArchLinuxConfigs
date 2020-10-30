6b54082369b7ae6870ae4f21ad2ec324224fbc7d
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2020-10-30 09:15:01 -0500

    Update ydn.sh

--- scripts/ydn.sh
+++ scripts/ydn.sh
@@ -6,8 +6,8 @@
 url=$@
 
 download_video () {
-	youtube-dl --continue --embed-subs --embed-thumbnail --add-metadata \
-    --merge-output-format mkv --ignore-errors --sub-lang en --write-sub \
+  youtube-dl --embed-subs --embed-thumbnail --add-metadata \
+    --merge-output-format mkv --ignore-errors --sub-lang en \
     --cookies ~/.config/youtube-dl/cookies.txt \
     --output "%(autonumber)s %(title)s (%(id)s).%(ext)s" "$1"
 }
