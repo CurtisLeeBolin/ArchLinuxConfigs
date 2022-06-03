e93d427ce2016e7126f61e86347234c49183c391
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2022-06-03 11:46:40 -0500

    Update add_default_settings.sh

--- scripts/add_default_settings.sh
+++ scripts/add_default_settings.sh
@@ -5,5 +5,5 @@ sudo pacman -S --noconfirm --needed bash bash-completion nano tmux
 cp ../configs/{.bashrc,.bash_profile,.nanorc,.tmux.conf} ~/
 sudo cp ../configs/{.bashrc,.bash_profile,.nanorc,.tmux.conf} /root/
 
-find /usr/share/nano/ -type f -name *.nanorc -exec echo 'include' "{}" >> ~/.nanorc \;
-sudo find /usr/share/nano/ -type f -name *.nanorc -exec echo 'include' "{}" >> /root/.nanorc \;
+find /usr/share/nano/ -type f -name *.nanorc -exec echo "include {}" >> ~/.nanorc \;
+sudo bash -c 'find /usr/share/nano/ -type f -name *.nanorc -exec echo "include {}" >> /root/.nanorc \;'
