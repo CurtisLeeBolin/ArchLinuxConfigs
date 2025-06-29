0f5e2c093068767d507aa651b307368c78201a4a
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2025-06-28 20:59:18 -0500

    Added add_default_settings_no_root.sh

---
+++ add_default_settings_no_root.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+configs=(
+  '.bashrc'
+  '.bash_profile'
+  '.nanorc'
+  '.tmux.conf'
+)
+
+for config in "${configs[@]}"; do
+  cp -v configs/${config} ~/
+done
+
+nano_syntax_highlights=(
+  $(find /usr/share/nano/ -type f -name *.nanorc)
+)
+
+for nano_syntax_highlight in "${nano_syntax_highlights[@]}"; do
+  echo "${nano_syntax_highlight}"
+  echo "include ${nano_syntax_highlight}" >> ~/.nanorc
+done
